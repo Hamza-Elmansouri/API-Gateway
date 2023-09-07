e8926fb2e913b2af37d15930ecbade6cf0fa869b
Hamza Elmansouri <hamza.elmansouri@usfoods.com>
2023-09-07 15:16:19 -0500

    Api-gateway

---
+++ src/main/java/com/ecomerce_data_model/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ecomerce_data_model.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
